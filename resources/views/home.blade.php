623eafa940091ac19d59852463ec5f0dda59a7ae
bt1k <6884455+bt1k@users.noreply.github.com>
2024-12-30 13:50:14 +0000

    Simplify home view, add home controller
    - The view for the home page (at the root path) has been renamed and simplified.
    - There is now a HomeController which passes a variable to the home view.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel Test</title>
+    </head>
+    <body>
+        <h1>Laravel Test</h1>
+        <p>Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</p>
+        <p>Random number from controller is: {{ $random }}</p>
+    </body>
+</html>
